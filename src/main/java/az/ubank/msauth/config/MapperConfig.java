f082a29593ecfbd1dccc8969f35d36add45b5c6e
Yunus Kazimov <yunus.kazimov@a2z.az>
2023-12-20 15:13:31 +0400

    add security configs add token service

---
+++ src/main/java/az/ubank/msauth/config/MapperConfig.java
@@ -0,0 +1,15 @@
+package az.ubank.msauth.config;
+
+import az.ubank.msauth.mapper.UserMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MapperConfig {
+
+    @Bean
+    public UserMapper userMapper() {
+        return UserMapper.INSTANCE;
+    }
+
+}
